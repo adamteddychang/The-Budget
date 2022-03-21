176c4418aaa02e13ff2119947b4a252fd5083bee
adamteddychang <adamchang0725@gmail.com>
2022-03-21 14:45:52 +0100

    added devise pages and root page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,10 @@
 class GroupsController < ApplicationController
+  
+  def index
+  end
+
+  def new 
+  end
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
+
+root 'groups#index'
 end
