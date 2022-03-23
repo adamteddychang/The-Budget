0359ce99b38f3c32b00d002132a0d23f1218f608
adamteddychang <adamchang0725@gmail.com>
2022-03-23 10:04:51 +0100

    added icons

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -38,6 +38,6 @@ class GroupsController < ApplicationController
   private
 
   def group_params
-    params.require(:group).permit(:name)
+    params.require(:group).permit(:name, :icon)
   end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ApplicationRecord
   belongs_to :user
-  has_and_belongs_to_many :spendings, dependent: :destroy
+  has_many :spendings, dependent: :destroy
 
   # validates :name, presence: true, length: { minimum: 3 }
 end
