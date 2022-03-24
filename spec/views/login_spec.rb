5e5f564d419ba64c11632c144e9739ca06e5a2d8
adamteddychang <adamchang0725@gmail.com>
2022-03-24 10:28:28 +0100

    added view-testing

---
+++ spec/views/login_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'User index page', type: :feature do
+  describe 'login' do
+    before(:each) do
+      User.create! password: '123456', email: 'user@email.com', name: 'User Name'
+      visit '/users/sign_in'
+    end
+
+    it 'can enter a name and receive a greeting' do
+      visit user_session_path
+      expect(page).to have_content 'LOGIN'
+    end
+
+    it 'user can see inputs and button' do
+      visit user_session_path
+      expect(page).to have_current_path(user_session_path)
+    end
+
+    it 'click the login error' do
+      visit user_session_path
+      click_button
+      expect(page).to have_content 'Invalid Email or password.'
+    end
+
+    it ' I cannot log in with incorrect user details' do
+      fill_in 'Email', with: 'usr@email.cm'
+      fill_in 'Password', with: '123456'
+      click_button 'Log in'
+      expect(page).to have_content 'Invalid Email or password'
+    end
+  end
+end
